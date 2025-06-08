1ed38b482ad6ac2f25d88958cdfd946d5abe6a97
YDMA970305 <ymartineza@unicartagena.edu.co>
2025-06-07 22:52:48 -0500

    se cambia el puerto a 9090 y se prueba el controlador de inicio

--- src/main/java/com/unidad3/articulo_spring/ControladorInicio.java
+++ src/main/java/com/unidad3/articulo_spring/ControladorInicio.java
@@ -1,5 +1,26 @@
 package com.unidad3.articulo_spring;
 
-public class ControladorInicio {
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+/*
+*
+* 
+    @autor "Yesid Martinez"
+*
+*
+*/
+@RestController
+@Slf4j
+public class ControladorInicio {
+    @GetMapping("/")
+    public String inicio() {
+        log.info("Ejecutando el controlador de inicio");
+        return "!Bienvenido a la API de Articulos!";
+    }
+
+   
 }
