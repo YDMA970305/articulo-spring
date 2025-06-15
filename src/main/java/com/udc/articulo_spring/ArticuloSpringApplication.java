ee1e54ff0ec1ae04eaa49f4af8ea870f12b76803
YDMA970305 <ymartineza@unicartagena.edu.co>
2025-06-15 12:00:19 -0500

    se crea el proyecto de Sprinboot con la estructura necesaria

---
+++ src/main/java/com/udc/articulo_spring/ArticuloSpringApplication.java
@@ -0,0 +1,13 @@
+package com.udc.articulo_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticuloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticuloSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udc/articulo_spring/ArticuloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udc.articulo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticuloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
